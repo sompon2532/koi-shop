cbbc70a8432d4470347a00757578e0ce5b3749fa
mmmai <nit.sukhom@gmail.com>
2017-10-31 16:30:40 +0700

    commit

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,41 +1,39 @@
 <!-- FOOTER -->
-    <footer>
-        <!-- <div class="footer-wrapper"> -->
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class ="footer-box">
-                            <div class="about-box text-center">
-                                <div class="row">
-                                    <div class="col-sm-2 col-sm-offset-5 col-md-2 col-md-offset-5">
-                                        <p class="text-thick footer-title"><a href="{{ url('about') }}">ABOUT US</a></p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <p>
-                                    KOIKICHI FISH FARM<br>
-                                    WE ARE A MEMVER OF<br>
-                                    ALL JAPAN NISHIKIGOI PROMOTION ASSOCIATION<br>
-                                    </p>
-                                </div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class ="footer-box">
+                    <div class="about-box text-center">
+                        <div class="row">
+                            <div class="col-sm-2 col-sm-offset-5 col-md-2 col-md-offset-5">
+                                <p class="text-thick footer-title"><a href="{{ url('about') }}">ABOUT US</a></p>
                             </div>
-                            <div class="contact-box text-center">
-                                <div class="row">
-                                    <div class="col-sm-2 col-sm-offset-5 col-md-2 col-md-offset-5">
-                                        <p class="text-thick footer-title"><a href="{{ url('contact') }}">CONTACT US</a></p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <p>
-                                    KOIKICHI FISH FARM<br>
-                                    120-122 ถ.พุทธมณฑลสาย 3 เเขวงทวีวัฒนา เขตทวีวัฒนา กรุงเทพมหานคร 10170 ประเทศไทย<br>
-                                    โทรศัพท์: 02-4315646 ต่อ 11 แฟกซ์: 02-4315646 มือถือ: 081-6427525<br>
-                                    </p>
-                                </div>
+                        </div>
+                        <div class="row">
+                            <p>
+                            KOIKICHI FISH FARM<br>
+                            WE ARE A MEMVER OF<br>
+                            ALL JAPAN NISHIKIGOI PROMOTION ASSOCIATION<br>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="contact-box text-center">
+                        <div class="row">
+                            <div class="col-sm-2 col-sm-offset-5 col-md-2 col-md-offset-5">
+                                <p class="text-thick footer-title"><a href="{{ url('contact') }}">CONTACT US</a></p>
                             </div>
+                        </div>
+                        <div class="row">
+                            <p>
+                            KOIKICHI FISH FARM<br>
+                            120-122 ถ.พุทธมณฑลสาย 3 เเขวงทวีวัฒนา เขตทวีวัฒนา กรุงเทพมหานคร 10170 ประเทศไทย<br>
+                            โทรศัพท์: 02-4315646 ต่อ 11 แฟกซ์: 02-4315646 มือถือ: 081-6427525<br>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-        <!-- </div> -->
-    </footer>
+        </div>
+    </div>
+</footer>
