3e208d9963ad53b832db4878807a764860ae72f6
Sompon Juicerod <sompon2532@gmail.com>
2018-05-26 12:51:33 +0700

    delete banner

--- app/Http/Controllers/Backoffice/BannerController.php
+++ app/Http/Controllers/Backoffice/BannerController.php
@@ -121,8 +121,10 @@ class BannerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Banner $banner)
     {
-        //
+        $banner->delete();
+
+        return;
     }
 }
