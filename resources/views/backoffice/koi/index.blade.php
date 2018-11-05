b6f930e98a814205fc60801f079ee1a1270acf8e
sompon juicerod <sompon2532@gmail.com>
2018-11-05 09:31:04 +0700

    set tabel horizontal

--- resources/views/backoffice/koi/index.blade.php
+++ resources/views/backoffice/koi/index.blade.php
@@ -21,70 +21,72 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-                <table id="datatable" class="table table-bordered table-hover">
-                    <thead>
-                    <tr>
-                        <th>KOI ID</th>
-                        <th>KOI</th>
-                        <th>Farm</th>
-                        <th>Variety</th>
-                        <th>Store</th>
-                        <th>Born</th>
-                        <th>Oyagoi</th>
-                        <th>Size</th>
-                        <th>Price</th>
-                        <th>Sex</th>
-                        <th>Unit</th>
-                        <th>Owner</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($kois as $index => $koi)
+                <div class="table-responsive">
+                    <table id="datatable" class="table table-bordered table-hover">
+                        <thead>
                         <tr>
-                            <td>{{ $koi->koi_id }}</td>
-                            <td>{{ $koi->name }}</td>
-                            <td>{{ $koi->farm ? $koi->farm->name : "" }}</td>
-                            <td>{{ $koi->strain ? $koi->strain->name : "" }}</td>
-                            <td>{{ $koi->store ? $koi->store->name : "" }}</td>
-                            <td>{{ $koi->born }}</td>
-                            <td>{{ $koi->oyagoi }}</td>
-                            <td>{{ count($koi->sizes) > 0 ? $koi->sizes->last()->size : "" }}</td>
-                            <td>{{ $koi->price }}</td>
-                            <td>{{ $koi->sex }}</td>
-                            <td>{{ $koi->unit }}</td>
-                            <td>{{ $koi->user ? $koi->user->name : 'Koikichi Farm'  }}</td>
-                            <td>{{ $koi->status ? 'Active' : 'Inactive' }}</td>
-                            <td>
-                                <a href="{{ route('koi.edit', ['koi' => $koi->id]) }}"
-                                   class="btn btn-warning btn-xs"><i class="fa fa-pencil-square-o"></i></a>
-                                <button data-token="{{ csrf_token() }}" data-id="{{ $koi->id }}" data-url="koi" class="btn-delete btn btn-danger btn-xs">
-                                    <i class="fa fa-trash-o"></i>
-                                </button>
-                            </td>
+                            <th>KOI ID</th>
+                            <th>KOI</th>
+                            <th>Farm</th>
+                            <th>Variety</th>
+                            <th>Store</th>
+                            <th>Born</th>
+                            <th>Oyagoi</th>
+                            <th>Size</th>
+                            <th>Price</th>
+                            <th>Sex</th>
+                            <th>Unit</th>
+                            <th>Owner</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <th>KOI ID</th>
-                        <th>KOI</th>
-                        <th>Farm</th>
-                        <th>Variety</th>
-                        <th>Store</th>
-                        <th>Born</th>
-                        <th>Oyagoi</th>
-                        <th>Size</th>
-                        <th>Owner</th>
-                        <th>Price</th>
-                        <th>Sex</th>
-                        <th>Unit</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                    </tfoot>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach ($kois as $index => $koi)
+                            <tr>
+                                <td>{{ $koi->koi_id }}</td>
+                                <td>{{ $koi->name }}</td>
+                                <td>{{ $koi->farm ? $koi->farm->name : "" }}</td>
+                                <td>{{ $koi->strain ? $koi->strain->name : "" }}</td>
+                                <td>{{ $koi->store ? $koi->store->name : "" }}</td>
+                                <td>{{ $koi->born }}</td>
+                                <td>{{ $koi->oyagoi }}</td>
+                                <td>{{ count($koi->sizes) > 0 ? $koi->sizes->last()->size : "" }}</td>
+                                <td>{{ $koi->price }}</td>
+                                <td>{{ $koi->sex }}</td>
+                                <td>{{ $koi->unit }}</td>
+                                <td>{{ $koi->user ? $koi->user->name : 'Koikichi Farm'  }}</td>
+                                <td>{{ $koi->status ? 'Active' : 'Inactive' }}</td>
+                                <td>
+                                    <a href="{{ route('koi.edit', ['koi' => $koi->id]) }}"
+                                       class="btn btn-warning btn-xs"><i class="fa fa-pencil-square-o"></i></a>
+                                    <button data-token="{{ csrf_token() }}" data-id="{{ $koi->id }}" data-url="koi" class="btn-delete btn btn-danger btn-xs">
+                                        <i class="fa fa-trash-o"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>KOI ID</th>
+                            <th>KOI</th>
+                            <th>Farm</th>
+                            <th>Variety</th>
+                            <th>Store</th>
+                            <th>Born</th>
+                            <th>Oyagoi</th>
+                            <th>Size</th>
+                            <th>Owner</th>
+                            <th>Price</th>
+                            <th>Sex</th>
+                            <th>Unit</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                <div class="table-responsive">
             </div>
             <!-- /.box-body -->
         </div>
