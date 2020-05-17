936ce52c384ba58efdd53c60b2fb2e899c9a498e
mmmai <nit.sukhom@gmail.com>
2020-05-18 00:37:16 +0700

    Chage Category View

--- resources/views/frontend/koi/category.blade.php
+++ resources/views/frontend/koi/category.blade.php
@@ -41,44 +41,61 @@
         <div class="col-md-12">
             @if(count($kois) > 0)
                 @foreach($kois as $index => $koi)
-                    <div class="col-xs-6 col-sm-6 col-md-3" style="margin-bottom:15px">
-                        <div class="stock-item-box ">
-                            <div class="thumbnail">
+                    <div class="col-xs-12 col-sm-6 col-md-3" style="margin-bottom:15px">
+                        <div class="stock-item-box">
+
+                            @if(count($koi->favorite))
+                                <span class="red-box-code">{{ $koi->koi_id }}</span>
+                            @else
+                                <span class="light-box-code">{{ $koi->koi_id }}</span>
+                            @endif
+
+                            <div class="thumbnail no-border">
                                 <a href="{{ route('frontend.koi.detail', ['id' => $koi->id]) }}">                                            
                                     @if(count($koi->media) > 0)  
-                                        <img src="{{ asset($koi->media->first()->getUrl()) }}" alt="..." class="image-responsive" style="max-height:150px;">
+                                        <img src="{{ asset($koi->media->first()->getUrl()) }}" alt="..." class="image-responsive" style="max-height:200px;">
                                     @else
-                                        <img src="{{ asset('frontend/src/img/default-koi.jpg') }}" alt="..." class="image-responsive" style="max-height:150px;">                                                
+                                        <img src="{{ asset('frontend/src/img/default-koi.jpg') }}" alt="..." class="image-responsive" style="max-height:200px;">                                                
                                     @endif
                                 </a>
-                                <div class="star-label">
-                                    @if(count($koi->favorite))
-                                        <form action="{{ route('frontend.user.favorite-del', ['id' => $koi->id, 'type' => 'App\Models\Koi']) }}" method="GET">  
-                                            <button type="submit" class="btn btn-favorite">
-                                                <img class="" src="{{ asset('frontend/src/img/unfavorite.png') }}" alt="unfavorite" style="max-height:50px;">    
-                                            </button> 
-                                            {{ csrf_field() }}
-                                        </form>
-                                    @else
-                                        <form action="{{ route('frontend.user.favorite-add', ['id' => $koi->id]) }}" method="GET">  
-                                            <input type="hidden" name="item" value="{{ $koi->id }}">
-                                            <input type="hidden" name="type" value="App\Models\Koi">
-                                            <button type="submit" class="btn btn-favorite">
-                                                <img class="" src="{{ asset('frontend/src/img/favorite.png') }}" alt="favorite" style="max-height:50px;">    
-                                            </button> 
-                                            {{ csrf_field() }}
-                                        </form>
-                                    @endif
-                                </div>
                             </div>  
+                            
                             <a href="{{ route('frontend.koi.detail', ['id' => $koi->id]) }}">
-                                <p class="text-red item-name-koi text-thick">{{ $koi->name }}</p>
-                                <p class="text-red">{{trans('koi.price')}} : {{ number_format($koi->price) }} {{trans('koi.thb')}}</p>
+                                @if(count($koi->favorite))
+                                    <span class="text-red item-name-koi text-thick">{{ $koi->name }}</span>
+                                @else
+                                    <span class="item-name-koi text-thick">{{ $koi->name }}</span>
+                                @endif
                             </a>
-                            {{--<!-- <p>{{trans('koi.code')}} : {{ $koi->koi_id }}</p> -->--}}
-                            {{--<!-- <a class="btn btn-white" href="{{ route('frontend.koi.detail', ['id' => $koi->id]) }}">
-                                {{trans('koi.btn-detail')}}
-                            </a> -->--}}
+
+                            @if(count($koi->favorite))
+                                <form action="{{ route('frontend.user.favorite-del', ['id' => $koi->id, 'type' => 'App\Models\Koi']) }}" method="GET" class="favorite-form">  
+                                    <button type="submit" class="btn-fav text-red"><i class="fa fa-star" aria-hidden="true"></i></button>
+                                    {{ csrf_field() }}
+                                </form>
+                            @else
+                                <form action="{{ route('frontend.user.favorite-add', ['id' => $koi->id]) }}" method="GET" class="favorite-form">  
+                                    <input type="hidden" name="item" value="{{ $koi->id }}">
+                                    <input type="hidden" name="type" value="App\Models\Koi">
+                                    <button type="submit" class="btn-fav"><i class="fa fa-star-o" aria-hidden="true""></i></button>
+                                    {{ csrf_field() }}
+                                </form>
+                            @endif
+
+                            <a href="{{ route('frontend.koi.detail', ['id' => $koi->id]) }}">
+                                <p style="color:#979797;font-size:18px">
+                                    <span>{{ trans('koi.born') }} : {{ strtoupper($koi->born) }}</span>
+                                    @if($koi->sex = "Male")
+                                        <span><i class="fa fa-mars" aria-hidden="true"></i></span>
+                                    @elseif($koi->sex = "Female")
+                                        <span><i class="fa fa-venus" aria-hidden="true"></i></span>
+                                    @else
+                                        <span><i class="fa fa-transgender" aria-hidden="true"></i></span>
+                                    @endif
+                                    <span>{{ strtoupper($koi->sizes->last()->size) }}</span></br>
+                                    <span style="color:#4A4A4A;font-size:20px">{{trans('koi.price')}} : {{ number_format($koi->price) }} {{trans('koi.thb')}}</span>
+                                </p>
+                            </a>
                         </div>
                     </div>
                 @endforeach
